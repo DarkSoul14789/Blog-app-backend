b8966f636a9961fee7b72f937048e13c5ca76ca7
DarkSoul14789 <shubhrajivshah@gmail.com>
2023-06-15 00:38:10 +0530

    Added custom exception and globalhandler

---
+++ src/main/java/com/shubh/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.shubh.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.shubh.blog.payloads.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+		String message = ex.getMessage();
+		ApiResponse apiResponse = new ApiResponse(false, message);
+		return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+	}
+	
+}
